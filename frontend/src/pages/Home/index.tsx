8f93db0d0cd393956c653feb0dee7b8e44606b17
Eduardo Souza <eduardosouzv@gmail.com>
2021-05-10 16:13:46 -0300

    feat: stylized homepage ;)

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 
 import { TopBar } from '../../components/TopBar';
 
+import gif from '../../assets/coffee.gif';
+
+import { Text, Button, Aligned } from './styles';
+
 const Home: React.FC = () => {
   return (
     <div>
       <TopBar />
+      <Text>From coffee lovers to coffee lovers...</Text>
+      <Aligned>
+        <Button href="#">I want a coffee</Button>
+        <img src={gif} alt="loading..." />
+      </Aligned>
     </div>
   );
 };
--- frontend/src/pages/Home/styles.ts
+++ frontend/src/pages/Home/styles.ts
@@ -1,9 +1,37 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  font-size: 2em;
-  color: #444b54;
+export const Text = styled.h1`
+  font-size: 3.5em;
+  color: #393939;
   font-family: Roboto;
-  display: inline;
-  margin-left: 16px;
+  width: 480px;
+
+  margin-top: 15vh;
+`;
+
+export const Button = styled.a`
+  padding: 30px 50px;
+  cursor: pointer;
+  border: 2px solid #444b54;
+  border-radius: 5px;
+  text-decoration: none;
+
+  font-family: Roboto, sans-serif;
+  font-size: 1.3em;
+  color: #393939;
+
+  &:hover {
+    background-color: #444b54;
+    color: white;
+    transition: 0.2s;
+  }
+`;
+
+export const Aligned = styled.div`
+  display: flex;
+  align-items: center;
+  img {
+    width: 400px;
+    margin-left: auto;
+  }
 `;
