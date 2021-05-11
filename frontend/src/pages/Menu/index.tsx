82d0b723185d32a8d5576a1b436af6ef2e683a80
Eduardo Souza <eduardosouzv@gmail.com>
2021-05-11 11:08:30 -0300

    feat: demo of menu

--- frontend/src/pages/Menu/index.tsx
+++ frontend/src/pages/Menu/index.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
 
-const Menu:React.FC  = () => {
-  return (<div>Menu</div>)
-}
+import { TopBar } from '../../components/TopBar';
+import { NavMenu } from '../../components/NavMenu';
+import { Item } from '../../components/Item';
+
+import { List } from './styles';
+
+import coffee from '../../assets/coffee-cup.png';
+
+const Menu: React.FC = () => {
+  return (
+    <div>
+      <TopBar />
+      <NavMenu />
+      <List>
+        <Item
+          title="product"
+          description="lorem ipsum lorem lorem lorem lorem loremlorem ipsum lorem lorem lorem lorem loremlorem ipsum lorem lorem lorem lorem lorem"
+          imgPath={coffee}
+        />
+        <Item
+          title="product"
+          description="lorem ipsum lorem lorem lorem lorem loremlorem ipsum lorem lorem lorem lorem loremlorem ipsum lorem lorem lorem lorem lorem"
+          imgPath={coffee}
+        />
+        <Item
+          title="product"
+          description="lorem ipsum lorem lorem lorem lorem loremlorem ipsum lorem lorem lorem lorem loremlorem ipsum lorem lorem lorem lorem lorem"
+          imgPath={coffee}
+        />
+        <Item
+          title="product"
+          description="lorem ipsum lorem lorem lorem lorem loremlorem ipsum lorem lorem lorem lorem loremlorem ipsum lorem lorem lorem lorem lorem"
+          imgPath={coffee}
+        />
+      </List>
+    </div>
+  );
+};
 
 export default Menu;
---
+++ frontend/src/pages/Menu/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const List = styled.div`
+  max-width: 800px;
+
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  margin: 20px auto;
+
+  div {
+    margin: 15px 40px;
+  }
+`;
