e293136e98949ff920106502775d5e03271bc7f3
Eduardo Souza <eduardosouzv@gmail.com>
2021-05-18 20:14:49 -0300

    feat: products dependencies moved to a context api

---
+++ frontend/src/contexts/ProductsContext.tsx
@@ -0,0 +1,56 @@
+import { createContext, Dispatch, ReactNode, SetStateAction, useState } from 'react';
+
+import api from '../config/api';
+
+interface ProviderProps {
+  children: ReactNode;
+}
+
+interface IProduct {
+  id: number;
+  title: string;
+  type: string;
+  description: string;
+  price: number;
+  imgUrl: string;
+}
+
+interface ContextData {
+  products: Array<IProduct>;
+  setProducts: Dispatch<SetStateAction<Array<IProduct>>>;
+  getAllProducts: () => void;
+  getHotCoffees: () => void;
+  getIceCoffees: () => void;
+}
+
+export const ProductsContext = createContext({} as ContextData);
+
+export const ProductsProvider = ({ children }: ProviderProps) => {
+  const [products, setProducts] = useState([] as Array<IProduct>);
+
+  const getAllProducts = () => {
+    api.get('/products/all').then(response => {
+      setProducts(response.data);
+    });
+  };
+
+  const getHotCoffees = () => {
+    api.get('/products/hot').then(response => {
+      setProducts(response.data);
+    });
+  };
+
+  const getIceCoffees = () => {
+    api.get('/products/ice').then(response => {
+      setProducts(response.data);
+    });
+  };
+
+  return (
+    <ProductsContext.Provider
+      value={{ products, setProducts, getAllProducts, getHotCoffees, getIceCoffees }}
+    >
+      {children}
+    </ProductsContext.Provider>
+  );
+};
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
+import { ProductsProvider } from './contexts/ProductsContext';
 
 ReactDOM.render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <ProductsProvider>
+        <App />
+      </ProductsProvider>
     </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root'),
