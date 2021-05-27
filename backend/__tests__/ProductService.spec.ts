05ea05995a09df6c116992a40bf50b3d5fbd7189
Eduardo Souza <eduardosouzv@gmail.com>
2021-05-26 21:54:42 -0300

    fiix: mock

--- backend/__tests__/ProductService.spec.ts
+++ backend/__tests__/ProductService.spec.ts
@@ -1,12 +1,30 @@
-import ProductsService from '../src/services/FakeProductsService';
+const mockProducts = [
+  {
+    id: 1,
+    title: 'French Cafe au Lait',
+    type: 'hot',
+    description:
+      'A bistro-style cafe au lait is oh-so-French and a fantastic way to greet a chilly morning.',
+    price: 4.33,
+    icon: 'icon',
+  },
+];
+
+jest.mock('../src/localProducts/products', () => {
+  return mockProducts;
+});
+
+import ProductsService from '../src/services/ProductsService';
 
 import { Product } from '../src/models/Product';
 
-describe('Products Service', () => {
-  it('should be able return a product from id 1', () => {
-    const service = new ProductsService();
+import { BASE_URL } from '../src/constants';
 
-    expect(service.getProductById(1, 'localhost:3333')).toStrictEqual(
+describe('Products Service', () => {
+  it('should return a product from id 1', () => {
+    const product = new ProductsService().getProductById(1);
+
+    expect(product).toEqual(
       new Product({
         id: 1,
         title: 'French Cafe au Lait',
@@ -14,8 +32,24 @@ describe('Products Service', () => {
         description:
           'A bistro-style cafe au lait is oh-so-French and a fantastic way to greet a chilly morning.',
         price: 4.33,
-        imgUrl: 'http://localhost:3333/products/photo?id=1',
+        imgUrl: `${BASE_URL}/products/photo?id=1`,
       }),
     );
+  });
+
+  it('should return all products', () => {
+    const products = new ProductsService().getAllProducts();
+
+    expect(products).toEqual([
+      {
+        id: 1,
+        title: 'French Cafe au Lait',
+        type: 'hot',
+        description:
+          'A bistro-style cafe au lait is oh-so-French and a fantastic way to greet a chilly morning.',
+        price: 4.33,
+        imgUrl: `${BASE_URL}/products/photo?id=1`,
+      },
+    ]);
   });
 });
