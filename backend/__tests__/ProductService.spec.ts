d93fbb8de0603bdd28a1292858d68b44a2411792
Eduardo Souza <eduardosouzv@gmail.com>
2021-05-27 17:33:46 -0300

    feat: unit tests

--- backend/__tests__/ProductService.spec.ts
+++ backend/__tests__/ProductService.spec.ts
@@ -6,7 +6,33 @@ const mockProducts = [
     description:
       'A bistro-style cafe au lait is oh-so-French and a fantastic way to greet a chilly morning.',
     price: 4.33,
-    icon: 'icon',
+    icon: 'path to icon in local files',
+  },
+  {
+    id: 2,
+    title: 'Cuban Coffee',
+    type: 'hot',
+    description: 'Cuban coffee is a sweet, rich, delicious hot beverage.',
+    price: 3.21,
+    icon: 'path to icon in local files',
+  },
+  {
+    id: 3,
+    title: 'Honey Almondmilk',
+    type: 'ice',
+    description:
+      'Lightly sweetened with honey and topped off with almondmilk for a balanced taste in every delicious sip.',
+    price: 5.28,
+    icon: 'path to icon in local files',
+  },
+  {
+    id: 4,
+    title: 'Salted Caramel Cream',
+    type: 'ice',
+    description:
+      'Super-smooth cold brew, sweetened with a touch of caramel and topped with a salted, rich cold foam.',
+    price: 6.13,
+    icon: 'path to icon in local files',
   },
 ];
 
@@ -49,6 +75,81 @@ describe('Products Service', () => {
           'A bistro-style cafe au lait is oh-so-French and a fantastic way to greet a chilly morning.',
         price: 4.33,
         imgUrl: `${BASE_URL}/products/photo?id=1`,
+      },
+      {
+        id: 2,
+        title: 'Cuban Coffee',
+        type: 'hot',
+        description: 'Cuban coffee is a sweet, rich, delicious hot beverage.',
+        price: 3.21,
+        imgUrl: `${BASE_URL}/products/photo?id=2`,
+      },
+      {
+        id: 3,
+        title: 'Honey Almondmilk',
+        type: 'ice',
+        description:
+          'Lightly sweetened with honey and topped off with almondmilk for a balanced taste in every delicious sip.',
+        price: 5.28,
+        imgUrl: `${BASE_URL}/products/photo?id=3`,
+      },
+      {
+        id: 4,
+        title: 'Salted Caramel Cream',
+        type: 'ice',
+        description:
+          'Super-smooth cold brew, sweetened with a touch of caramel and topped with a salted, rich cold foam.',
+        price: 6.13,
+        imgUrl: `${BASE_URL}/products/photo?id=4`,
+      },
+    ]);
+  });
+
+  it('should return all coffees of type hot', () => {
+    const hotCoffees = new ProductsService().getHotCoffes();
+
+    expect(hotCoffees).toEqual([
+      {
+        id: 1,
+        title: 'French Cafe au Lait',
+        type: 'hot',
+        description:
+          'A bistro-style cafe au lait is oh-so-French and a fantastic way to greet a chilly morning.',
+        price: 4.33,
+        imgUrl: `${BASE_URL}/products/photo?id=1`,
+      },
+      {
+        id: 2,
+        title: 'Cuban Coffee',
+        type: 'hot',
+        description: 'Cuban coffee is a sweet, rich, delicious hot beverage.',
+        price: 3.21,
+        imgUrl: `${BASE_URL}/products/photo?id=2`,
+      },
+    ]);
+  });
+
+  it('should return all coffees of type ice', () => {
+    const iceCoffees = new ProductsService().getIceCoffes();
+
+    expect(iceCoffees).toEqual([
+      {
+        id: 3,
+        title: 'Honey Almondmilk',
+        type: 'ice',
+        description:
+          'Lightly sweetened with honey and topped off with almondmilk for a balanced taste in every delicious sip.',
+        price: 5.28,
+        imgUrl: `${BASE_URL}/products/photo?id=3`,
+      },
+      {
+        id: 4,
+        title: 'Salted Caramel Cream',
+        type: 'ice',
+        description:
+          'Super-smooth cold brew, sweetened with a touch of caramel and topped with a salted, rich cold foam.',
+        price: 6.13,
+        imgUrl: `${BASE_URL}/products/photo?id=4`,
       },
     ]);
   });
