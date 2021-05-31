e0f140dc0550a335a9e1e1a3fc3a2a4a31459ede
Eduardo Souza <eduardosouzv@gmail.com>
2021-05-31 20:04:01 -0300

    fix: users route

--- backend/src/routes/router.ts
+++ backend/src/routes/router.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import ProductRouter from './ProductRouter';
+import UserRouter from './UserRouter';
 
 const routes = Router();
 
 routes.use('/products', ProductRouter);
+routes.use('/user', UserRouter);
 
 export default routes;
