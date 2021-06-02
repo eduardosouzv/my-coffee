60cc94caaeaa7751e0df9d6022965f0a6cfd4324
Eduardo Souza <eduardosouzv@gmail.com>
2021-06-02 08:20:52 -0300

    fix: import and catch error

--- backend/src/controllers/UsersController.ts
+++ backend/src/controllers/UsersController.ts
@@ -1,20 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
 
 import UserService from '../services/UserService';
-const asyncHandler = require('express-async-handler');
+import asyncHandler from 'express-async-handler';
 
 export const createUser = asyncHandler(
-  (request: Request, response: Response, next: NextFunction) => {
+  async (request: Request, response: Response, next: NextFunction) => {
     const { user, password } = request.body;
 
-    const service = new UserService().createNewUser({ user, password });
-
-    service
-      .then(res =>
-        response.json({
-          message: `user ${res} created`,
-        }),
-      )
-      .catch(err => next(err));
+    try {
+      const createdUser = await new UserService().createNewUser({ user, password });
+      response.status(201).json({
+        message: `user ${createdUser} created`,
+      });
+    } catch (err) {
+      next(err);
+    }
   },
 );
