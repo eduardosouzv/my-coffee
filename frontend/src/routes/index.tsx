c638aa9cd23973f43353b41b4540671334811c02
Eduardo Souza <eduardosouzv@gmail.com>
2021-06-18 17:43:56 -0300

    feat: route to checkout

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,17 +2,16 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Menu from '../pages/Menu';
+import Checkout from '../pages/Checkout';
 
 import { PrivateRoute } from './PrivateRoute';
-
-const checkout = () => <h1>checkout</h1>;
 
 const Routes = () => {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/menu" component={Menu} />
-      <PrivateRoute path="/teste" component={checkout} />
+      <PrivateRoute path="/checkout" component={Checkout} />
     </Switch>
   );
 };
